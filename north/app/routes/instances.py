4c19729bdc2e7bbd800296e6b3f9cec50d25829f
Sherjeel Shabih <shabihsherjeel@gmail.com>
2021-10-08 11:59:04 +0200

    Added paths mounting to the launch endpoint

--- north/app/routes/instances.py
+++ north/app/routes/instances.py
@@ -17,17 +17,20 @@
 #
 
 from typing import List
+from pathlib import Path
 from fastapi import APIRouter, HTTPException, Request, Depends
 from collections import deque
 from datetime import datetime, timedelta
 import time
 import docker
 from docker import DockerClient
+from docker.types import Mount
 
 from fastapi import APIRouter
 from fastapi.params import Depends
 
 from north import config
+from north.auth import create_channel_token
 from north.app.routes.auth import token_launch
 from north.app.models import InstanceModel, InstanceResponseModel, ChannelUnavailableError
 
@@ -69,6 +72,13 @@ def get_available_channel() -> str:
         ) from channel_unavailable
 
 
+def get_docker_mounts_from_paths(paths):
+    mounts = []
+    for path in paths:
+        mounts.append(Mount(target='/home/jovyan/work/' + Path(path).name, source=path, type='bind'))
+    return mounts
+
+
 @router.get(
     '/',
     tags=[router_tag],
@@ -100,6 +110,7 @@ async def get_instances():
 async def post_instances(request: Request, instance: InstanceModel, token=Depends(token_launch)):
     ''' Create a new tool instance. '''
     channel = get_available_channel()
+    channel_token = create_channel_token(int(channel)).token
     path = f'{request.scope.get("root_path")}/container/{channel}/'
     container_name = f'{config.docker_name_prefix}-{token.token}-{instance.name}'
 
@@ -110,7 +121,8 @@ async def post_instances(request: Request, instance: InstanceModel, token=Depend
     current_containers = docker_client.containers.list(**docker_name_prefix_filter)
     if len(current_containers) != 0:
         path = current_containers[0].labels['path']
-        return InstanceResponseModel(path=path)
+        channel_token = current_containers[0].labels['channel_token']
+        return InstanceResponseModel(path=path, channel_token=channel_token)
 
     docker_client.containers.run(
         image="jupyter/datascience-notebook",
@@ -123,7 +135,8 @@ async def post_instances(request: Request, instance: InstanceModel, token=Depend
         name=container_name,
         user="1000:1000",
         group_add=["1000"],
-        labels={"path": path}
+        labels={"path": path, "channel_token": channel_token},
+        mounts=get_docker_mounts_from_paths(instance.paths)
     )
 
-    return InstanceResponseModel(path=path)
+    return InstanceResponseModel(path=path, channel_token=channel_token)
